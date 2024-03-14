4da9c9aca82358dd7e2c54052b55c13c78b2e61d
Fabricio C Santos <fsantos.veredinha@gmail.com>
2024-03-13 23:07:25 -0300

    Subindo alterações
    implementação do navigation, e preview da tela inicial

--- app/src/main/java/br/com/fiap/geoworldmania/telaInicial/telaInicialView.kt
+++ app/src/main/java/br/com/fiap/geoworldmania/telaInicial/telaInicialView.kt
@@ -1,4 +1,20 @@
 package br.com.fiap.geoworldmania.telaInicial
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+
+@Composable
+fun telaInicial(){
+    //Adicionar parâmetro -navController: NavController- após não precisar do preview;
+
+    
 
 
+}
+
+@Preview
+@Composable
+fun telaInicialPreview(){
+    telaInicial()
+}
