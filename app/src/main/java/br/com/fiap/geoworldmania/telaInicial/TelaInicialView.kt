e8a44fe74c7ff61d60e6869f0e688dab0a485062
Fabricio C Santos <fsantos.veredinha@gmail.com>
2024-03-16 09:13:32 -0300

    Mudanças atoa

---
+++ app/src/main/java/br/com/fiap/geoworldmania/telaInicial/TelaInicialView.kt
@@ -0,0 +1,19 @@
+package br.com.fiap.geoworldmania.telaInicial
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TelaInicial(){
+    //Adicionar parâmetro | navController: NavController | após não precisar do preview;
+
+
+
+
+}
+
+@Preview
+@Composable
+fun TelaInicialPreview(){
+    TelaInicial()
+}
